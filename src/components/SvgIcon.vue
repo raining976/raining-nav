7bd2707c8aa6eeaebb36e17fad10f0bf60403299
raining <running.zhang@foxmail.com>
2023-12-02 00:13:27 +0800

    feat:封装svg图标组件

---
+++ src/components/SvgIcon.vue
@@ -0,0 +1,39 @@
+<template>
+    <svg :class="svgClass" aria-hidden="true">
+        <use :xlink:href="iconClassName" />
+    </svg>
+</template>
+<script setup>
+import { computed } from 'vue';
+const props = defineProps({
+    iconName: {
+        type: String,
+        required: true
+    },
+    className: {
+        type: String,
+        default: 'i-icon'
+    }
+})
+
+
+const iconClassName = computed(() => {
+    return `#${props.iconName}`;
+})
+
+const svgClass = computed(() => {
+    return props.className ? `i-icon ${props.className}` : "i-icon";
+})
+
+</script>
+
+<style scoped lang="scss">
+.i-icon {
+    width: 1em;
+    height: 1em;
+    min-width: 1em;
+    position: relative;
+    fill: currentColor;
+    vertical-align: -2px;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -8,8 +8,10 @@ import store from './store'
 // import icon and icon styles
 import {install} from '@icon-park/vue-next/es/all';
 import '@icon-park/vue-next/styles/index.css';
+// import custom components
+import SvgIcon from "@/components/SvgIcon.vue";
 
 const app = createApp(App)
 install(app,'i'); // use custom prefix ,usage: i-iconName
-
+app.component('SvgIcon',SvgIcon)
 app.use(store).mount('#app')
