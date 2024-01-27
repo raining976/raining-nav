36c56d46010dee7bb9468525ae9edb55131f7413
raining <running.zhang@foxmail.com>
2024-01-27 22:06:30 +0800

    fix：jsnop无法解决部署跨域（服务端问题），换用其他获取ip的方法
    此方法无法判断客户端是否代理

--- src/api/index.js
+++ src/api/index.js
@@ -1,14 +1,8 @@
 import axios from "axios";
 
-
-
-
 // 获取一言的请求
 export const getOneSentence = async () => {
   const url = "https://v1.hitokoto.cn/?encode=js&select=%23hitokoto"
-  // await fetchData(url,"hitokoto","encode=js&select=%23hitokoto").then(res=>{
-  //   eval(res)
-  // })
   const script = document.createElement("script");
   script.src = url;
   script.defer = true
@@ -17,30 +11,6 @@ export const getOneSentence = async () => {
   return script
 };
 
-function fetchData(base_url,cbName,queryStr='') {
-  return new Promise((resolve, reject) => {
-    cbName = `${cbName}`
-    var url = base_url
-    if(queryStr=='') url = `${base_url}?callback=${cbName}`
-    else url = `${base_url}?${queryStr}&callback=${cbName}`
-    const script = document.createElement("script");
-    script.src = url
-    script.async = true
-    window[cbName] = (data) => {
-      // console.log('data',data)
-      delete window[cbName]
-      document.body.removeChild(script);
-      resolve(data);
-    }
-    script.onerror = (error) => {
-      delete window[cbName];
-      document.body.removeChild(script);
-      reject(error);
-    };
-
-    document.body.appendChild(script);
-  })
-}
 
 /**
  * 获取天气信息
@@ -49,15 +19,13 @@ function fetchData(base_url,cbName,queryStr='') {
  * https://lbs.amap.com/api/webservice/guide/api/weatherinfo
  */
 export const getWeather = async () => {
-  const getIpUrl = "https://whois.pconline.com.cn/ipJson.jsp"
-  var regionData = await fetchData(getIpUrl,'IPCallBack')
-  const cityCode = regionData.reginCode || regionData.cityCode
   const gaodeKey = '5af2bbd8702291fc7df7015d94e8c19e' // 高德地图的web服务api
+  const ip = await axios.get('https://api.ipify.org/').then(res=>res.data)
+  const regionData = await axios.get(`https://restapi.amap.com/v3/ip?key=${gaodeKey}&ip=${ip}`).then(res=>res.data)
+  const cityCode = regionData.adcode
   const weatherUrl = `https://restapi.amap.com/v3/weather/weatherInfo?key=${gaodeKey}&city=${cityCode}`
   return await axios.get(weatherUrl).then(res => res = res.data.lives[0]).catch(error => console.error(error))
 }
-
-
 
 function jsonp(url,params = {}) {
   return new Promise((resolve, reject) => {
